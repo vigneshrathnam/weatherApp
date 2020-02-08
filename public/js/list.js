c21914a80fdc1f9e901018a4ece4c57fe8e624d1
Vignesh015 <vigneshrathnams@gmail.com>
2020-02-08 13:16:37 +0530

    Initial commit

--- public/js/list.js
+++ public/js/list.js
@@ -1,25 +1,17 @@
 const data=document.getElementById("data");
-var xhr;
-if ("XMLHttpRequest" in window) {
-    // code for modern browsers
-    xhr = new XMLHttpRequest();
- } else {
-    // code for old IE browsers
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-xhr.onreadystatechange=()=>{
-  if(xhr.status == 200 && xhr.readyState == 4){
-	  var html="<tbody><tr><th rowspan='2'>S.No.</th><th rowspan='2'>City / Country</th><th rowspan='2'>City / Country ID</th><th colspan=2>City / Country Coordinates</th><tr><th> lat</th><th> lon</th></tr>";
-	  var response=JSON.parse(xhr.responseText);
-	  html+=`<tr><td>1</td><td>${response[0].name} ( ${response[0].country} )</td><td>${response[0].id}</td><td>${response[0].coord.lat}</td><td>${response[0].coord.lon}</td></tr>`;
+data.innerHTML="Loading...";
+axios.get("/city.list.min.json")
+  .then((res)=>{
+	 if(res.status>=200 && res.status<400) {
+	  var html="<thead class='thead-light'><tr><th rowspan='2'>S.No.</th><th rowspan='2'>City / Country</th><th rowspan='2'>City / Country ID</th><th colspan=2>City / Country Coordinates</th><tr><th> lat</th><th> lon</th></tr></thead><tbody>";
+	  html+=`<tr><td>1</td><td>${res.data[0].name} ( ${res.data[0].country} )</td><td>${res.data[0].id}</td><td>${res.data[0].coord.lat}</td><td>${res.data[0].coord.lon}</td></tr>`;
+	  html+=`<tr><td>1</td><td>${res.data[1].name} ( ${res.data[1].country} )</td><td>${res.data[1].id}</td><td>${res.data[1].coord.lat}</td><td>${res.data[1].coord.lon}</td></tr>`;
 	  data.innerHTML=html+"</tbody>";
-  }
-  else{
-	data.innerHTML="<div class=\"text-danger\">Failed to Load<br/>Status:"+xhr.status+"</div>";
 }
-	
-};
+ 	else{ 
+	data.innerHTML="<div class=\"text-danger\">Failed to Load<br/>Status:"+res.status+"</div>";
 
-xhr.open("GET","/city.list.min.json",false);
-xhr.send();
+}
+
+	
+});
