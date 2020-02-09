73464bd3c10bd97fbb302e82adf7ce707161bcd8
Vignesh015 <vigneshrathnams@gmail.com>
2020-02-09 14:30:55 +0530

    Initial Commit

--- public/js/list.js
+++ public/js/list.js
@@ -1,12 +1,14 @@
 const data=document.getElementById("data");
 data.innerHTML="Loading...";
-axios.get("/city.list.min.json")
+axios.get("/city")
   .then((res)=>{
 	 if(res.status>=200 && res.status<400) {
-	  var html="<thead class='thead-light'><tr><th rowspan='2'>S.No.</th><th rowspan='2'>City / Country</th><th rowspan='2'>City / Country ID</th><th colspan=2>City / Country Coordinates</th><tr><th> lat</th><th> lon</th></tr></thead><tbody>";
-	  html+=`<tr><td>1</td><td>${res.data[0].name} ( ${res.data[0].country} )</td><td>${res.data[0].id}</td><td>${res.data[0].coord.lat}</td><td>${res.data[0].coord.lon}</td></tr>`;
-	  html+=`<tr><td>1</td><td>${res.data[1].name} ( ${res.data[1].country} )</td><td>${res.data[1].id}</td><td>${res.data[1].coord.lat}</td><td>${res.data[1].coord.lon}</td></tr>`;
-	  data.innerHTML=html+"</tbody>";
+	  var html="";
+	  data.innerHTML="Please Wait for a while";
+	  res.data.data.map((data,index)=>{
+	  html+=`<tr><td>${index+1}</td><td>${data.name} ( ${data.country} )</td><td>${data.id}</td><td>${data.coord.lat}</td><td>${data.coord.lon}</td></tr>`;  
+});
+	 data.innerHTML=html;
 }
  	else{ 
 	data.innerHTML="<div class=\"text-danger\">Failed to Load<br/>Status:"+res.status+"</div>";
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const router=express.Router();
+const axios=require("axios");
 
 router.get("/",(req,res)=>{
   res.render("index");
@@ -17,5 +18,45 @@ router.get("/uvindex",(req,res)=>{                      res.render("uvindex");
 
 router.get("/citylist",(req,res)=>{                      res.render("citylist");
 });
+
+router.get("/city",(req,res)=>{
+	var { limit,page }=req.query;
+	limit=parseInt(limit) || 100;
+	page=parseInt(page) || 1;
+	var results={};
+	var startIndex=(page-1)*limit;
+	var endIndex=page*limit;
+
+	axios.get("/city.list.min.json", { proxy: { host: '127.0.0.1', port: 5000 } }) 
+        .then((data)=>{
+		  results.size=data.data.length;
+		  result=data.data.splice(startIndex,endIndex);
+		if(startIndex<result.length){
+		  results.next={ page: page+1,
+			limit
+		  }
+
+		}
+
+		if(endIndex>0){
+                  results.previous={ page: page-1,
+                        limit
+                  }                                        
+                }
+		results.data=result;
+		
+		res.json(results);
+
+
+
+	  })
+	.catch((e)=>{
+		console.log(e);
+		res.json(e);
+	}
+	);
+	
+});
+
 
 module.exports=router;
