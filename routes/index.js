1920cffb213f29fa516444fdf81fd17065a8a776
Vignesh015 <vigneshrathnams@gmail.com>
2020-02-13 22:48:22 +0530

    init commit

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,16 @@ router.get("/weather",(req,res)=>{
 	res.render("weather");
 });
 
-router.get("/forecast",(req,res)=>{                      res.render("forecast");
+router.get("/forecast",(req,res)=>{                
+	res.render("forecast");
 });
 
-router.get("/uvindex",(req,res)=>{                      res.render("uvindex");
+router.get("/uvindex",(req,res)=>{                   
+	res.render("uvindex");
 });
 
-router.get("/citylist",(req,res)=>{                      res.render("citylist");
+router.get("/citylist",(req,res)=>{                   
+	res.render("citylist");
 });
 
 router.get("/city",(req,res)=>{
